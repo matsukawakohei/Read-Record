901e9c7a15fb56e8bfe4b68a723c89f32989ffd8
Kohei Matsukawa <kohei.matsukawa@gmail.com>
2022-11-27 02:17:27 +0900

    [fix] ルーティングを定義 残作業 ・コンポーネントファイルの作成

--- src/src/router/index.js
+++ src/src/router/index.js
@@ -1,19 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home.vue'
+/**
+ * @todo
+ * コンポーネントを作成
+ */
+import BookSearch from '../views.BookSearch.vue'
+import BookForm from '../views.BookForm.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Home',
+    component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/search',
+    name: 'Search',
+    component: BookSearch
+  },
+  {
+    path: '/form',
+    name: 'Form',
+    component: BookForm
+  },
+  {
+    path: '/:paths(.*)*',
+    name: 'Failback',
+    redirect: '/'
   }
 ]
 
